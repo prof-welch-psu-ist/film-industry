05a7eb23d3bb539ac33a166979bf8f4bfb2aad09
dtwelch <dtw.welch@gmail.com>
2024-10-02 17:02:54 -0400

    add some rough UML

--- src/FilmIndustry.java
+++ src/FilmIndustry.java
@@ -61,13 +61,43 @@ public class FilmIndustry {
 
     // a record type to represent a movie
     public record Movie(String title, Director director, Producer producer, int yearReleased,
-                     MPAARating rating, ArrayList<Actor> actors) {
+                     MPAARating rating, ArrayList<Actor> actors, MovieGenre g) implements Comparable<Movie> {
+
+        public int countNumberOfImprovActorsInMovie() {
+            int result = 0;
+            for (Actor a : actors) {
+                if (a.style() == ActingStyle.Improv) {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+        @Override public int compareTo(Movie o) {
+            return this.yearReleased - o.yearReleased;
+        }
     }
 
     public static void main(String[] args) {
 
-        //var producer = new P
-        Movie terminator2 = new Movie("Terminator 2", )
+        Producer producer = new Producer("J", "Abrams", 15);
+        Director director = new Director("Steven", "S", 30, true);
+
+        ArrayList<Actor> actors = new ArrayList<>();
+
+        actors.add(new Actor("Arnold", "S", ActingStyle.Improv, 40));
+        actors.add(new Actor("Sarah", "C", ActingStyle.Method, 50));
+        actors.add(new Actor("Stephen", "S", ActingStyle.Method, 3));
+        Movie terminator2 = new Movie("Terminator 2", director, producer, 1994, MPAARating.R, actors, MovieGenre.Action);
+        // ^^^^^^ unwieldy when creating a movie (too many things to pass in...)
+        // would be a good place for a builder...
+        /*
+        Movie terminator3 = new MovieBuilder("Terminator 3")
+                .addActor(a1)
+                .addActor(a2)
+                .rating(MPAARating.R).genre(MovieGenre.Horror)
+                .build();
+         */
 
     }
 }
